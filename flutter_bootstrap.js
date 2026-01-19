0eff7b06e0bb26de7b03882e72e4d6d494861350
Asiful600w <Asiful600w@users.noreply.github.com>
2026-01-19 06:49:28 +0000

    deploy: 439bbe747340145011fa6c33aecdd4224dbdad42

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2862329876"
+    serviceWorkerVersion: "2490467031"
   }
 });
